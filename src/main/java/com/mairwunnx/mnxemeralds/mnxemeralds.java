d45d714e49ab6c890e4d8f07dc461ef4a68dc53a
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-08-05 20:40:49 +0300

    Updated logo image and version.

--- src/main/java/com/mairwunnx/mnxemeralds/mnxemeralds.java
+++ src/main/java/com/mairwunnx/mnxemeralds/mnxemeralds.java
@@ -23,7 +23,7 @@ public class mnxemeralds
     private static Logger logger;
     public static final String MODID = "mnxemeralds";
     public static final String NAME = "mnxemeralds";
-    public static final String VERSION = "1.0.0.0";
+    public static final String VERSION = "1.0.1.0";
 
     @SidedProxy(clientSide = "com.mairwunnx.mnxemeralds.proxy.ClientProxy", serverSide = "com.mairwunnx.mnxemeralds.proxy.CommonProxy")
     public static CommonProxy proxy;
