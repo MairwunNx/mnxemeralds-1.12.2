3d11fecb409f2a055b3e9a9910038c199a673b46
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-08-05 19:52:15 +0300

    Added explosion on right click on super sword.

--- src/main/java/com/mairwunnx/mnxemeralds/items/emeraldsupersword.java
+++ src/main/java/com/mairwunnx/mnxemeralds/items/emeraldsupersword.java
@@ -1,11 +1,23 @@
 package com.mairwunnx.mnxemeralds.items;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityLargeFireball;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 import net.minecraftforge.common.util.EnumHelper;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.List;
+
+import static com.mairwunnx.mnxemeralds.registers.itemregister.KEY27;
 
 public class emeraldsupersword extends ItemSword
 {
@@ -17,6 +29,27 @@ public class emeraldsupersword extends ItemSword
         this.setRegistryName(name);
         this.setUnlocalizedName(name);
         this.canRepair = true;
+        addInformation(null, null, "Don't use at home!!!");
+
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+    {
+        ItemStack item = playerIn.getHeldItem(handIn);
+        Vec3d aim = playerIn.getLookVec();
+        EntityLargeFireball fireball = new EntityLargeFireball(worldIn, playerIn, 1, 1,1);
+
+        fireball.setPosition(playerIn.posX + aim.x * 1.5D, playerIn.posY + aim.y * 1.5D, playerIn.posZ + aim.z * 1.5D);
+        fireball.accelerationX = aim.x * 0.1;
+        fireball.accelerationY = aim.y * 0.1;
+        fireball.accelerationZ = aim.z * 0.1;
+        fireball.explosionPower = 13;
+
+        worldIn.spawnEntity(fireball);
+
+        item.damageItem(1, playerIn);
+        return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, item);
     }
 
     @SideOnly(Side.CLIENT)
