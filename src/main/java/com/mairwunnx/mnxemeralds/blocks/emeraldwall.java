9121471d84ff47af65bd7307856a8b63218a5919
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-08-08 20:00:58 +0300

    Added furnace (not work).

--- src/main/java/com/mairwunnx/mnxemeralds/blocks/emeraldwall.java
+++ src/main/java/com/mairwunnx/mnxemeralds/blocks/emeraldwall.java
@@ -3,8 +3,6 @@ package com.mairwunnx.mnxemeralds.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockWall;
 
-import static com.mairwunnx.mnxemeralds.mnxemeralds.defaultModTab;
-
 public class emeraldwall extends BlockWall
 {
     public emeraldwall(String name, float resistance, float hardness, Block modelBlock)
@@ -13,7 +11,6 @@ public class emeraldwall extends BlockWall
         setUnlocalizedName(name);
         setRegistryName(name);
         setResistance(resistance);
-        setCreativeTab(defaultModTab);
         setHardness(hardness);
         this.useNeighborBrightness = true;
         setLightLevel(20.0F);
